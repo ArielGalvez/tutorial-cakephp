eb53084dda5d364fc97a1dfbe2fb324cea2a2f9c
Ariel Galvez <arielgp.70@gmail.com>
2023-10-31 18:52:45 -0400

    fix migrations and generate seed

--- app/config/Seeds/UsersArticlesSeed.php
+++ app/config/Seeds/UsersArticlesSeed.php
@@ -1,11 +1,24 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\AbstractSeed;
 
-class InsertData extends AbstractMigration
+/**
+ * UsersArticles seed.
+ */
+class UsersArticlesSeed extends AbstractSeed
 {
-    public function change()
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run(): void
     {
         // Insert data into the 'users' table
         $usersData = [

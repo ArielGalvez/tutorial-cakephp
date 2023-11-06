bfd3f385168983981d84e2b211ab4213b0d4251d
ArielGalvez <arielgp.70@gmail.com>
2023-11-06 04:05:10 -0400

    remove harcoded id

--- app/templates/Articles/add.php
+++ app/templates/Articles/add.php
@@ -1,8 +1,6 @@
 <h1>Add Article</h1>
 <?php
     echo $this->Form->create($article);
-    // TODO: user_id Hard code for now. we going to fix it soon.
-    echo $this->Form->control('user_id', ['type' => 'hidden', 'value' => 2]);
     echo $this->Form->control('title');
     echo $this->Form->control('body', ['rows' => '3']);
     echo $this->Form->control('tag_string', ['type' => 'text']);
